9234144628ba8a982acb94fbf33f9b72e7345075
yvespracticum <yvveeessss@ya.ru>
2025-02-02 19:00:16 +0300

    Combine all in one app - foodapp

--- backend/foodgram/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-    path('auth/', include('djoser.urls.authtoken')),
-    path('users/', include('users.urls')),
-
-]
--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from foodapp.views import FoodgramUserViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', FoodgramUserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/users/', include(router.urls)),
 ]
